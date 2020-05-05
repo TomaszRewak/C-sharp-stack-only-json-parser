2c0aaf274523f291b2365cdc8f515702353c78cb
Tomasz Rewak <tomaszrewak@gmail.com>
2020-05-05 20:02:12 +0200

    StackOnlyString

---
+++ StackOnlyJsonParser/StackOnlyString.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace StackOnlyJsonParser
+{
+    public ref struct StackOnlyString
+    {
+        private readonly Utf8JsonReader _jsonReader;
+
+        public bool HasValueSequence => _jsonReader.HasValueSequence;
+        public ReadOnlySpan<byte> ValueSpan => _jsonReader.ValueSpan;
+        public ReadOnlySequence<byte> ValueSequence => _jsonReader.ValueSequence;
+
+        public string Value => _jsonReader.GetString();
+
+        internal StackOnlyString(Utf8JsonReader jsonReader)
+        {
+            _jsonReader = jsonReader;
+        }
+    }
+}
