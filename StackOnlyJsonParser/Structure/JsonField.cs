fac0679f82db089d547b6f2c9aa460022b05a9c2
Tomasz Rewak <tomaszrewak@gmail.com>
2020-05-07 22:00:00 +0200

    Structure

---
+++ StackOnlyJsonParser/Structure/JsonField.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackOnlyJsonParser.Structure
+{
+    internal sealed class JsonField
+    {
+    }
+}
---
+++ StackOnlyJsonParser/Structure/JsonType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackOnlyJsonParser.Structure
+{
+    internal sealed class JsonType
+    {
+        public string TypeName { get; set; }
+
+        public List<JsonField> Fields { get; set; } = new List<JsonField>();
+    }
+}
