eba2d09929628f73968c1b9a186a2be9d6221275
Tomasz Rewak <tomaszrewak@gmail.com>
2020-05-13 18:39:39 +0200

    Improved tests.

--- StackOnlyJsonParser.PerformanceTests/Program.cs
+++ StackOnlyJsonParser.PerformanceTests/Program.cs
@@ -10,149 +10,104 @@ namespace StackOnlyJsonParser.PerformanceTests
 {
 	class Program
 	{
+		private static Stopwatch stopwatch = new Stopwatch();
+		private static decimal price;
+
 		static void Main(string[] args)
 		{
 			var dataGenerator = new DataGenerator();
-			var data = dataGenerator.Generate(1000000);
+			var data = dataGenerator.Generate(int.Parse(args[1]));
+			var stringData = Encoding.UTF8.GetString(data);
 
-			Console.WriteLine("Start");
+			for (int i = 0; i < 5; i++)
+			{
+				GC.Collect();
+				var allocatedBytes = GC.GetAllocatedBytesForCurrentThread();
+				var usedBytes = GC.GetTotalMemory(false);
 
-			TestHeap1(Encoding.UTF8.GetString(data));
-			TestHeap2(data);
-			TestStack(data);
-			TestStackOnly(data);
-			TestHeap1(Encoding.UTF8.GetString(data));
-			TestHeap2(data);
-			TestStack(data);
-			TestStackOnly(data);
+				price = 0;
+
+				switch (args[0])
+				{
+					case "Newtonsoft":
+						TestHeap1(stringData);
+						break;
+					case "System.Text.Json":
+						TestHeap2(data);
+						break;
+					case "Stack":
+						TestStack(data);
+						break;
+					case "StackOnly":
+						TestStackOnly(data);
+						break;
+				}
+
+				allocatedBytes = GC.GetAllocatedBytesForCurrentThread() - allocatedBytes;
+				usedBytes = GC.GetTotalMemory(false) - usedBytes;
+
+				Console.WriteLine($"{price}\t{args[0]}\t{args[1]}\t{stopwatch.ElapsedMilliseconds}\t{allocatedBytes}\t{usedBytes}");
+			}
 		}
 
 		static void TestHeap1(string data)
 		{
-			GC.Collect();
-			var bytes = GC.GetAllocatedBytesForCurrentThread();
-			var usedBytes = GC.GetTotalMemory(false);
-			var stopwatch = Stopwatch.StartNew();
+			stopwatch.Restart();
 			var products = JsonConvert.DeserializeObject<List<HeapProduct>>(data);
-			var price = 0m;
 
 			foreach (var product in products)
-			{
 				foreach (var color in product.Colors)
-				{
 					if (color == "red")
-					{
 						foreach (var region in product.Regions)
-						{
 							if (region.Key == "PL")
-							{
 								price += product.AvailableItems * region.Value.Value;
-								break;
-							}
-						}
-						break;
-					}
-				}
-			}
 
 			stopwatch.Stop();
-			Console.WriteLine($"{price} {stopwatch.Elapsed} \t{GC.GetAllocatedBytesForCurrentThread() - bytes} \t{GC.GetTotalMemory(false) - usedBytes}");
 		}
 
 		static void TestHeap2(ReadOnlySpan<byte> data)
 		{
-			GC.Collect();
-			var bytes = GC.GetAllocatedBytesForCurrentThread();
-			var usedBytes = GC.GetTotalMemory(false);
-			var stopwatch = Stopwatch.StartNew();
+			stopwatch.Restart();
 			var products = JsonSerializer.Deserialize<List<HeapProduct>>(data);
-			var price = 0m;
 
 			foreach (var product in products)
-			{
 				foreach (var color in product.Colors)
-				{
 					if (color == "red")
-					{
 						foreach (var region in product.Regions)
-						{
 							if (region.Key == "PL")
-							{
 								price += product.AvailableItems * region.Value.Value;
-								break;
-							}
-						}
-						break;
-					}
-				}
-			}
 
 			stopwatch.Stop();
-			Console.WriteLine($"{price} {stopwatch.Elapsed} \t{GC.GetAllocatedBytesForCurrentThread() - bytes} \t{GC.GetTotalMemory(false) - usedBytes}");
 		}
 
 		static void TestStack(ReadOnlySpan<byte> data)
 		{
-			GC.Collect();
-			var bytes = GC.GetAllocatedBytesForCurrentThread();
-			var usedBytes = GC.GetTotalMemory(false);
-			var stopwatch = Stopwatch.StartNew();
+			stopwatch.Restart();
 			var products = new StackProducts(data);
-			var price = 0m;
 
 			foreach (var product in products)
-			{
 				foreach (var color in product.Colors)
-				{
 					if (color == "red")
-					{
 						foreach (var region in product.Regions)
-						{
 							if (region.Key == "PL")
-							{
 								price += product.AvailableItems * region.Value.Value;
-								break;
-							}
-						}
-						break;
-					}
-				}
-			}
 
 			stopwatch.Stop();
-			Console.WriteLine($"{price} {stopwatch.Elapsed} \t{GC.GetAllocatedBytesForCurrentThread() - bytes} \t{GC.GetTotalMemory(false) - usedBytes}");
 		}
 
 		static void TestStackOnly(ReadOnlySpan<byte> data)
 		{
-			GC.Collect();
-			var bytes = GC.GetAllocatedBytesForCurrentThread();
-			var usedBytes = GC.GetTotalMemory(false);
-			var stopwatch = Stopwatch.StartNew();
+			stopwatch.Restart();
 			var products = new StackOnlyProducts(data);
-			var price = 0m;
 
 			foreach (var product in products)
-			{
 				foreach (var color in product.Colors)
-				{
 					if (color.ValueTextEquals("red"))
-					{
 						foreach (var region in product.Regions)
-						{
 							if (region.Key.ValueTextEquals("PL"))
-							{
 								price += product.AvailableItems * region.Value.Value;
-								break;
-							}
-						}
-						break;
-					}
-				}
-			}
 
 			stopwatch.Stop();
-			Console.WriteLine($"{price} {stopwatch.Elapsed} \t{GC.GetAllocatedBytesForCurrentThread() - bytes} \t{GC.GetTotalMemory(false) - usedBytes}");
 		}
 	}
 }
