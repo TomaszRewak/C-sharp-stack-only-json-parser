5b0b6659f5dbd660f3468524cade47b7e2c8bff6
Tomasz Rewak <tomaszrewak@gmail.com>
2020-05-17 17:30:20 +0200

    Performance tests results.

--- StackOnlyJsonParser.PerformanceTests/run.ps1
+++ StackOnlyJsonParser.PerformanceTests/run.ps1
@@ -1,6 +1,6 @@
 foreach($algorithm in 'Newtonsoft', 'System.Text.Json', 'Stack', 'StackOnly')
 {
-    foreach($elements in 1, 100, 1000, 1000000, 1500000, 2500000)
+    foreach($elements in 1, 100, 1000, 10000, 100000, 200000, 500000, 1000000, 1500000, 2500000)
     {
         dotnet.exe "bin\Release\net5.0\StackOnlyJsonParser.PerformanceTests.dll" $algorithm $elements
     }
